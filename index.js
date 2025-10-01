b53587543d763e8bf8555d3b333d77754764aad8
Gabriel Melo <gabriel.ferreira@construtorapatriani.com.br>
2025-10-01 10:16:38 -0300

    feat: add the functions for the game

---
+++ index.js
@@ -0,0 +1,54 @@
+let humanScore = 0
+let computerScore = 0
+
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    let computerChoice = ""
+
+    if (randomNumber === 0) {
+        computerChoice = "pedra";
+    }
+    if (randomNumber === 1) {
+        computerChoice = "tesoura";
+    }
+    if (randomNumber === 2) {
+        computerChoice = "papel";
+    }
+
+    return computerChoice
+}
+
+function getHumanChoice() {
+    let choice = prompt("Pedra, Papel ou Tesoura?");
+
+    return choice.toLowerCase();
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(humanSelection, computerSelection) {
+
+    if (humanSelection === computerSelection) {
+        console.log("Empate!")
+    } else if (humanSelection === "pedra" && computerSelection == "tesoura") {
+        console.log("Você ganhou o round!")
+        humanScore++
+    } else if (humanSelection === "papel" && computerSelection == "pedra") {
+        console.log("Você ganhou o round!")
+        humanScore++;
+    } else if (humanSelection === "tesoura" && computerSelection == "papel") {
+        console.log("Você ganhou o round!")
+        humanScore++;
+    } else {
+        console.log("Você perdeu o round!")
+        computerScore++;
+    }
+    console.log("sua seleção:", humanSelection)
+    console.log("seleção do pc:", computerSelection)
+    console.log(humanScore, computerScore)
+}
+
+playRound(humanSelection, computerSelection);
+
+
